760a14de9f618df80e13ed218989fc31ba9b5cc6
marcos <marcosgabriel89355@gmail.com>
2025-05-01 20:05:14 -0300

    melhoria no responsivo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,13 +4,16 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Impacto360',
-  description: 'Inovação em Marketing e Mídia!',
+  title: 'Impacto360 | Marketing, Inovação e Resultados',
+  description: 'Somos a ponte entre ideias criativas e resultados reais. A Impacto360 transforma visões em experiências digitais que conectam marcas ao futuro.',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={inter.className + " bg-preto-profundo text-white"}>
         {children}
       </body>
